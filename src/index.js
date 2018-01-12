2eff91af6ac2474d6b22a73fdc5ece42e013694c
shashi <sasitech18@gmail.com>
2018-01-12 03:09:19 +0000

    Finished Project with all the code

---
+++ src/index.js
@@ -0,0 +1,51 @@
+/*
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { provider } from 'react-redux';
+
+const App = <h1>Redux Shopping Cart</h1>
+
+ReactDOM.render (
+ <Provider store={store}>
+    { App }
+    </Provider>,
+    document.getElementById('root')
+);
+
+*/
+
+import store from './store.js';
+import { addToCart, updateCart, deleteFromCart }  from './actions/cart-actions';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+const App = <h1>Redux Shopping Cart</h1>;
+
+ReactDOM.render(
+  <Provider store={store}>
+    { App }
+  </Provider> ,
+  document.getElementById('root')
+);
+
+// console.log("initial state: ", store.getState());
+//
+// let unsubscribe = store.subscribe(() =>
+//   console.log(store.getState())
+// );
+
+// Add to Cart
+store.dispatch(addToCart('Coffee 500gm', 1, 250));
+store.dispatch(addToCart('Flour 1kg', 2, 110));
+store.dispatch(addToCart('Juice 2L', 1, 250));
+
+// Update Cart
+store.dispatch(updateCart('Flour 1kg', 5, 110));
+
+// Delete from Cart
+store.dispatch(deleteFromCart('Coffee 500gm'));
+
+// unsubscribe();
+
+console.log('Install Redux DevTools Chrome Extension to view output!');
